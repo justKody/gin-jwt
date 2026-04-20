505197f345b5c3c908dab9c97e5e3a0689ff0654
smarth <smarthverma2003@gmail.com>
2026-04-21 00:05:29 +0530

    Update environment variables, enhance user login functionality, and implement JWT token generation. Added SECRET_KEY to .env, updated go.mod and go.sum for new dependencies, and refactored userController and tokenHelper for improved authentication flow.

--- controllers/userController.go
+++ controllers/userController.go
@@ -78,8 +78,7 @@ func SignUp() gin.HandlerFunc {
 		user.Refresh_token = &refreshToken
 
 		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
- 
-		
+
 		if insertErr != nil {
 			msg := fmt.Sprintf("User item was not created")
 			c.JSON(http.StatusInternalServerError, gin.h{"error": msg})
@@ -92,7 +91,37 @@ func SignUp() gin.HandlerFunc {
 
 func Login() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "Login method"})
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "email or password is incorrect"})
+			return
+		}
+
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+
+		if passwordIsValid != true {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		if foundUser.Email == nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user not found"})
+		}
+
+		token, refreshToken, _ := helper.GenerateAllTokens(*foundUser.Email, *foundUser.First_name, *foundUser.Last_name, *foundUser.User_type, foundUser.User_id)
+
+		// c.JSON(200, gin.H{"success": "Login method"})
 	}
 }
 
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,16 +1,56 @@
-package helpers
+package helper
 
 import (
+	"log"
+	"os"
 	"time"
+
+	"github.com/Kiteretzu/gin-jwt/database"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// later
-func GenerateAllTokens(email string, firstName string, lastName string, userType string, userId string) (signedToken string, signedRefreshToken string, err error) {
-	claims := &model.SignedDetails{
-		Email: email,
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	User_type  string
+	jwt.RegisteredClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var SECRET_KEY = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:      email,
 		First_name: firstName,
-		Last_name: lastName,
-		User_type: userType,
-		User_id: userId,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
 	}
+
+	refreshClaims := &SignedDetails{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(168 * time.Hour)),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
 }
